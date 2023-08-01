d3c42ce0d1b398d4499718dd1ef6ac2422d565ee
Volodymyr Bodnar <v.bodnar@levi9.com>
2023-08-01 09:47:55 +0300

    31.07.2023

--- module03/lesson01/01_thread_like_function.py
+++ module03/lesson01/01_thread_like_function.py
@@ -5,7 +5,6 @@ def worker(param):
     print(param)
 
 
-if __name__ == '__main__':
-    for i in range(5):
-        th = Thread(target=worker, args=(f"Count thread - {i}", ))
-        th.start()
+for i in range(5):
+    th = Thread(target=worker, args=(f"Count thread - {i}", ))
+    th.start()
--- module03/lesson01/03_thread_timer.py
+++ module03/lesson01/03_thread_timer.py
@@ -8,14 +8,29 @@ def worker(param):
     logging.debug(param)
 
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, format="%(threadName)s %(message)s")
-    one = Timer(0.5, worker, args=('one param', ))
-    one.name = 'First thread'
-    one.start()
-    two = Timer(1.5, worker, args=('two param', ))
-    two.name = 'Second thread'
-    two.start()
-    sleep(1)
-    two.cancel()
-    logging.debug('End program')
+# if __name__ == '__main__':
+#     logging.basicConfig(level=logging.DEBUG, format="%(threadName)s %(message)s")
+#     one = Timer(0.5, worker, args=('one param', ))
+#     one.name = 'First thread'
+#     one.start()
+#     two = Timer(1.5, worker, args=('two param', ))
+#     two.name = 'Second thread'
+#     two.start()
+#     sleep(1)
+#     two.cancel()
+#     logging.debug('End program')
+
+
+
+def check_confirmation():
+    print('checking')
+    print('user confirmed...')
+    return True
+
+def register(user: dict):
+    print(user.get("name"))
+    print("sending mail...")
+    checker = Timer(15,  check_confirmation)
+    checker.start()
+
+register({"name": "JD"})
